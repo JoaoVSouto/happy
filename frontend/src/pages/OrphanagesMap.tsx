3f7db4ed2e31b6d93924221e913829f411a99f6e
JoaoVSouto <joaovitorss99@hotmail.com>
2020-10-13 00:22:59 -0300

    [FRONTEND] Adds Leaflet map into OrphanagesMap

--- frontend/src/pages/OrphanagesMap.tsx
+++ frontend/src/pages/OrphanagesMap.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Map, TileLayer } from 'react-leaflet';
 import { FiPlus } from 'react-icons/fi';
+
+import 'leaflet/dist/leaflet.css';
 
 import '../styles/pages/orphanages-map.scss';
 
@@ -23,7 +26,15 @@ const OrphanagesMap: React.FC = () => {
         </footer>
       </aside>
 
-      <div></div>
+      <Map
+        center={[-5.8474576, -35.2076127]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+      </Map>
 
       <Link to="/" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
