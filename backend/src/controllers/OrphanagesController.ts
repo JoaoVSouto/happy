311d400561a61c0137e699f022c53f06a116a232
JoaoVSouto <joaovitorss99@hotmail.com>
2020-10-13 17:37:14 -0300

    [BACKEND] Adds controller for listing orphanages

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -4,6 +4,14 @@ import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
 
 class OrphanagesController {
+  static async index(req: Request, res: Response) {
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanages = await orphanagesRepository.find();
+
+    return res.json(orphanages);
+  }
+
   static async create(req: Request, res: Response) {
     const {
       name,
