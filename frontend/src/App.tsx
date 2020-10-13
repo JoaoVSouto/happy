87ad5c52f6bd78762fbe2d55201050e874bb8ff7
JoaoVSouto <joaovitorss99@hotmail.com>
2020-10-12 23:06:33 -0300

    [FRONTEND] Creates global and landing page styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+import './styles/global.scss';
+import './styles/pages/landing.scss';
+
+import logoImg from './assets/images/logo.svg';
 
 const App: React.FC = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Natal</strong>
+          <span>Rio Grande do Norte</span>
+        </div>
+
+        <a href="#!" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default App;
