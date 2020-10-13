11cffa28fb5166ecaadded37b5544be197cdf987
JoaoVSouto <joaovitorss99@hotmail.com>
2020-10-12 23:37:33 -0300

    [FRONTEND] Styles OrphanagesMap's aside and add link

--- frontend/src/pages/OrphanagesMap.tsx
+++ frontend/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import '../styles/pages/orphanages-map.scss';
+
+import mapMarkerImg from '../assets/images/map-marker.svg';
 
 const OrphanagesMap: React.FC = () => {
-  return <h1>Orphanages map</h1>;
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita &#58;&#41;</p>
+        </header>
+
+        <footer>
+          <strong>Natal</strong>
+          <span>Rio Grande do Norte</span>
+        </footer>
+      </aside>
+
+      <div></div>
+
+      <Link to="/" className="create-orphanage">
+        <FiPlus size={32} color="#fff" />
+      </Link>
+    </div>
+  );
 };
 
 export default OrphanagesMap;
