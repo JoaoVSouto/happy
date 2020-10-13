6cf7bce7c2a5debdcabd3f228790be50cbfc9b00
JoaoVSouto <joaovitorss99@hotmail.com>
2020-10-13 17:22:14 -0300

    [BACKEND] Abstracts app logic on routes and controllers

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+
+import Orphanage from '../models/Orphanage';
+
+class OrphanagesController {
+  static async create(req: Request, res: Response) {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    } = req.body;
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = orphanagesRepository.create({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    });
+
+    await orphanagesRepository.save(orphanage);
+
+    return res.status(201).json(orphanage);
+  }
+}
+
+export default OrphanagesController;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,41 +1,14 @@
 import express from 'express';
-import { getRepository } from 'typeorm';
-
-import Orphanage from './models/Orphanage';
 
 import './database/connection';
+
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/orphanages', async (req, res) => {
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  } = req.body;
-
-  const orphanagesRepository = getRepository(Orphanage);
-
-  const orphanage = orphanagesRepository.create({
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  });
-
-  await orphanagesRepository.save(orphanage);
-
-  return res.json(orphanage);
-});
+app.use(routes);
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server listening on ${PORT}...`));
