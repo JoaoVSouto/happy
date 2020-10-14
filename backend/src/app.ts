b532fa3b28b0fb17075b727adc675946b01aaba3
JoaoVSouto <joaovitorss99@hotmail.com>
2020-10-13 22:29:07 -0300

    [BACKEND] Reestructures main app file

---
+++ backend/src/app.ts
@@ -0,0 +1,42 @@
+import 'express-async-errors';
+
+import path from 'path';
+
+import express, { Express } from 'express';
+import cors from 'cors';
+
+import './database/connection';
+
+import routes from './routes';
+import errorHandler from './errors/handler';
+
+class App {
+  public readonly server: Express;
+
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+    this.exceptionHandler();
+  }
+
+  private middlewares() {
+    this.server.use(cors());
+    this.server.use(express.json());
+    this.server.use(
+      '/uploads',
+      express.static(path.join(__dirname, '..', 'uploads'))
+    );
+  }
+
+  private routes() {
+    this.server.use(routes);
+  }
+
+  private exceptionHandler() {
+    this.server.use(errorHandler);
+  }
+}
+
+export default new App().server;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,24 +1,4 @@
-import path from 'path';
-import express from 'express';
-import cors from 'cors';
-
-import 'express-async-errors';
-
-import './database/connection';
-
-import routes from './routes';
-
-import errorHandler from './errors/handler';
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
-
-app.use(routes);
-
-app.use(errorHandler);
+import app from './app';
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server listening on ${PORT}...`));
