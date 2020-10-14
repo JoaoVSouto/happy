fff2040323374b97c4ac296bb2950387b31f203f
JoaoVSouto <joaovitorss99@hotmail.com>
2020-10-13 21:59:04 -0300

    [BACKEND] Adds error handler

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,12 @@
 import path from 'path';
 import express from 'express';
+import 'express-async-errors';
 
 import './database/connection';
 
 import routes from './routes';
+
+import errorHandler from './errors/handler';
 
 const app = express();
 
@@ -11,6 +14,8 @@ app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 app.use(routes);
+
+app.use(errorHandler);
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server listening on ${PORT}...`));
