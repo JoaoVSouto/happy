6a65cd1db8c47506f08cc71e5a7c989d0cbaaf88
JoaoVSouto <joaovitorss99@hotmail.com>
2020-10-17 22:04:53 -0300

    [MOBILE] Styles marker callout

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { StyleSheet, View, Dimensions } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+
+import mapMarker from './src/assets/images/map-marker.png';
 
 export default function App() {
   return (
@@ -14,7 +16,25 @@ export default function App() {
           latitudeDelta: 0.008,
           longitudeDelta: 0.008,
         }}
-      />
+      >
+        <Marker
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 3.2,
+            y: 0.9,
+          }}
+          coordinate={{
+            latitude: -5.8850813,
+            longitude: -35.2038551,
+          }}
+        >
+          <Callout tooltip onPress={() => {}}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Lar das meninas</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
     </View>
   );
 }
@@ -27,5 +47,19 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
+    justifyContent: 'center',
+  },
+
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14,
   },
 });
