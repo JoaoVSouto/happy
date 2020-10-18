d777cae6b6d5366a327d70e7f6a1267115e7739c
JoaoVSouto <joaovitorss99@hotmail.com>
2020-10-18 00:57:07 -0300

    [MOBILE] Posts orphanage data to API

--- mobile/src/pages/CreateOrphanage/OrphanageData.tsx
+++ mobile/src/pages/CreateOrphanage/OrphanageData.tsx
@@ -9,10 +9,12 @@ import {
   TouchableOpacity,
   Image,
 } from 'react-native';
-import { useRoute } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import * as ImagePicker from 'expo-image-picker';
 import { Feather } from '@expo/vector-icons';
 import { RectButton } from 'react-native-gesture-handler';
+
+import api from '../../services/api';
 
 interface IOrphanageDataRouteParams {
   position: {
@@ -25,6 +27,8 @@ export default function OrphanageData() {
   const route = useRoute();
   const params = route.params as IOrphanageDataRouteParams;
 
+  const navigation = useNavigation();
+
   const [name, setName] = useState('');
   const [about, setAbout] = useState('');
   const [instructions, setInstructions] = useState('');
@@ -32,18 +36,30 @@ export default function OrphanageData() {
   const [open_on_weekends, setOpenOnWeekends] = useState(true);
   const [images, setImages] = useState<string[]>([]);
 
-  function handleCreateOrphanage() {
+  async function handleCreateOrphanage() {
     const { latitude, longitude } = params.position;
 
-    console.log({
-      name,
-      about,
-      instructions,
-      opening_hours,
-      open_on_weekends,
-      latitude,
-      longitude,
+    const data = new FormData();
+
+    data.append('name', name);
+    data.append('about', about);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('instructions', instructions);
+    data.append('opening_hours', opening_hours);
+    data.append('open_on_weekends', String(open_on_weekends));
+
+    images.forEach((image, index) => {
+      data.append('images', {
+        name: `image_${index}.jpg`,
+        type: 'image/jpg',
+        uri: image,
+      } as any);
     });
+
+    await api.post('orphanages', data);
+
+    navigation.navigate('OrphanagesMap');
   }
 
   async function handleSelectImages() {
